db58b3ebacb8cd48f2c6d23614afc32ec4c7d391
Nulman37 <63564949+Nulman37@users.noreply.github.com>
2020-05-18 21:01:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-
+let BudgetCategory = function(pName, pBudgetTarget){
+    this.name = pName;
+    this.BudgetTarget = pBudgetTarget;
+}
 
 // Now comes the code that must wait to run until the document is fully loaded
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  
+    let categoryArray = [];
+
+    document.getElementById("addCategory").addEventListener("click", function () {
+        let budgetNameValue = document.getElementById("budgetName").value;
+        let budgetTargetValue = document.getElementById("budgetTarget").value;
+
+        let temp = new BudgetCategory(budgetNameValue, budgetTargetValue);
+
+        categoryArray.push(temp)
+        displayCategory();
+        document.getElementById("form-1").reset();
+        document.getElementById("budgetName").focus();
+    });
+
+    let displayCategory = function() {
+        for (let i in categoryArray){
+            document.getElementById("categoryList")
+        }
+    }
+
 });  
-
-
-
